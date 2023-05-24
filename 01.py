2cb2fc0b67ad52d01fc1dcd3afc4f1d1d861d8f6
elahe <elaheghaderi37@gmail.com>
2023-05-24 10:56:30 +0430

    stock_web

---
+++ 01.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+st.write("""
+# Stock Market Web Application 
+**Visually**  show data on a stock! Data range from Jan 2, 2022-Aug 4,2020
+
+""")
+
+#image=Image.open()
+
+
